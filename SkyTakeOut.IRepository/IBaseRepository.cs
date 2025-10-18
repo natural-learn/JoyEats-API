2702fc0af9cb23ca0909d853b59ee6924545545a
目迪 刘 <L269852198@163.com>
2025-10-18 18:09:42 +0800

    新增仓储

---
+++ SkyTakeOut.IRepository/IBaseRepository.cs
@@ -0,0 +1,145 @@
+﻿using SkyTakeOut.Common;
+using System.Linq.Expressions;
+
+namespace SkyTakeOut.IRepository
+{
+    /// <summary>
+    /// 空接口，用于类型约束
+    /// </summary>
+    public interface IBaseRepository
+    {
+
+    }
+
+    public interface IBaseRepository<TEntity> : IBaseRepository where TEntity : class
+    {
+        #region 新增
+        /// <summary>
+        /// 插入数据
+        /// </summary>
+        /// <param name="entity">要插入的实体</param>
+        /// <param name="cancellationToken">取消异步操作</param>
+        /// <returns></returns>
+        Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 批量插入数据
+        /// </summary>
+        /// <param name="entities">要插入的数据集合</param>
+        /// <param name="cancellationToken">取消异步操作</param>
+        /// <returns></returns>
+        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+        #endregion
+
+        #region 删除
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="entity">要删除的实体</param>
+        /// <param name="cancellationToken">取消异步操作</param>
+        /// <returns></returns>
+        void DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 根据主键删除数据
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="id"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task DeleteByIdAsync<TKey>(TKey id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 批量删除数据
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        void DeleteRangeAsync(IEnumerable<TEntity> entities);
+        #endregion
+
+        #region 更新
+        /// <summary>
+        /// 更新数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        void Update(TEntity entity);
+        #endregion
+
+        #region 查询
+        /// <summary>
+        /// 根据主键查询数据
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="id"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<TEntity?> GetByIdAsync<TKey>(TKey id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 根据条件返回符合条件的第一个数据，如果没有则返回null
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 返回符合条件的所有数据
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<List<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 返回符合条件的所有数据并排序
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="isAscending"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<List<TEntity>> GetListAsync(Expression<Func<TEntity, bool>>? predicate,
+                                         Expression<Func<TEntity, object>>? orderBy,
+                                         bool isAscending = true,
+                                         CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 分页查询
+        /// </summary>
+        /// <param name="predicate">查询条件</param>
+        /// <param name="orderBy">排序表达式</param>
+        /// <param name="pageIndex">页码（从1开始）</param>
+        /// <param name="pageSize">每页大小</param>
+        /// <param name="isAscending">是否升序</param>
+        /// <param name="cancellationToken">取消异步操作</param>
+        /// <returns></returns>
+        Task<PagedResult<TEntity>> GetPagedListAsync(Expression<Func<TEntity, bool>>? predicate,
+                                                     Expression<Func<TEntity, object>>? orderBy,
+                                                     int pageIndex = 1,
+                                                     int pageSize = 10,
+                                                     bool isAscending = true,
+                                                     CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 返回符合条件的数量
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+        #endregion
+
+        /// <summary>
+        /// 执行原生SQL语句，返回受影响的行数。
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        Task<int> ExecuteSqlAsync(string sql, CancellationToken cancellationToken = default, params object[] parameters);
+
+    }
+}
---
+++ SkyTakeOut.IRepository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using SkyTakeOut.Core.Autofac.DependencyInjection;
+using System.Data;
+
+namespace SkyTakeOut.IRepository.UnitOfWork
+{
+    public interface IUnitOfWork : IScopeDependency, IDisposable
+    {
+        /// <summary>
+        /// 获取通用仓储
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
+        IBaseRepository<TEntity> GetBaseRepository<TEntity>() where TEntity : class;
+
+        /// <summary>
+        /// 获取具体仓储
+        /// </summary>
+        /// <typeparam name="TRepository"></typeparam>
+        /// <returns></returns>
+        TRepository GetRepository<TRepository>() where TRepository : IBaseRepository;
+
+        /// <summary>
+        /// 保存更改
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        /**
+         * EFCore的DbContext在调用SaveChanges或SaveChangesAsync方法时会自动开启和提交事务，并在以下情况自动回滚：
+         * 1. 执行过程中抛出异常（如数据库约束冲突、网络错误等）
+         * 2. 未调用 SaveChangesAsync（所有内存中的修改不会提交到数据库）
+         */
+
+        /// <summary>
+        /// 手动开启事务
+        /// </summary>
+        /// <param name="isolationLevel"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<IDbContextTransaction> BeginTransactionAsync(
+            IsolationLevel isolationLevel = IsolationLevel.ReadCommitted,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 提交手动开启的事务
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 回滚手动开启的事务
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
+    }
+}
